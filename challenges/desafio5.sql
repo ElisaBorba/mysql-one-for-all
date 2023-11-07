d5e1c16d146695f6198b5cd28f616b73e993521b
ElisaBorba <elisaborbaa@gmail.com>
2023-11-07 02:36:04 -0300

    req 04

---
+++ challenges/desafio5.sql
@@ -0,0 +1,9 @@
+SELECT
+  TRIM('"' FROM c.cancao_name) AS cancao,
+  COUNT(h.cancao_id) AS reproducoes
+FROM historico_de_reproducoes AS h
+INNER JOIN cancoes AS c
+ON h.cancao_id = c.cancao_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao ASC
+LIMIT 2;
