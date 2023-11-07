7e14ff93fdaf13194d28f8a5758d07679f017eda
ElisaBorba <elisaborbaa@gmail.com>
2023-11-07 02:37:08 -0300

    req 05

--- challenges/desafio5.sql
+++ challenges/desafio5.sql
@@ -1,9 +1,9 @@
 SELECT
-  TRIM('"' FROM c.cancao_name) AS cancao,
+  c.cancao_name AS cancao,
   COUNT(h.cancao_id) AS reproducoes
 FROM historico_de_reproducoes AS h
 INNER JOIN cancoes AS c
 ON h.cancao_id = c.cancao_id
-GROUP BY cancao
+GROUP BY c.cancao_name
 ORDER BY reproducoes DESC, cancao ASC
 LIMIT 2;
